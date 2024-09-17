6e6fd4341453983b2e273ff8fd5b75be5878de60
Hamdi <TLiLiOS@yahoo.com>
2024-09-17 18:20:16 +0200

    Initial Commit

---
+++ NetWorking/NetWorkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetWorkingApp.swift
+//  NetWorking
+//
+//  Created by MacBook Air on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetWorkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
